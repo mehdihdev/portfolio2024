ac35e8fac8c68b36e5f6f0cef57c583747d67401
Mehdi Hussain <mehdi@mehdi.us>
2024-11-13 14:26:17 -0800

    add script library

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <script src="https://kit.fontawesome.com/ea12dcf2e6.js" crossorigin="anonymous"></script>
+      <Script src="https://kit.fontawesome.com/ea12dcf2e6.js" crossorigin="anonymous"></Script>
         </Head>
       <body className="antialiased">
         <Main />
